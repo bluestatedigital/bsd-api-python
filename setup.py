23c4c43e188aef59291173fa75c7693145bfb05b
Brendan Maione-Downing <b.maionedowning@gmail.com>
2018-10-07 09:39:40 -0400

    Fix tox and travis

--- setup.py
+++ setup.py
@@ -74,7 +74,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=[
